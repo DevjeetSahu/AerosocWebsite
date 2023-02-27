b4622b314e3c5d50cd6663525a550eb6a2014d78
Roopeshchowdary <98178960+Roopeshchowdary@users.noreply.github.com>
2023-02-27 22:04:19 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import Alumn from "./components_alumn/Alumn.js";
-import Achievements from "./components_alumn/Alumn_achievement.js";
+import SignIn from "./Components_Web/SignIn.js";
 
-
-const achieves = ["peter pan peter","Meinu Daku Daku...","ambarsariya lageya.","Naatu Naatu",]
-
-const values=[
-  {person_pic:"",person_name:"r",person_desc:"abcdef"},{person_pic:"",person_name:"r",person_desc:"abcdef"},{person_pic:"",person_name:"r",person_desc:"abcdef"},{person_pic:"",person_name:"r",person_desc:"abcdef"},]
-
-
-
-  function App() {
+function App() {
   return (
-    <div className="alumni_page">
-      <div className="information_alumns">
-        <Alumn value={values}/>
-      </div>
-      <div className="alumn_achievements">
-        <Achievements achieves={achieves} />
-      </div>
-    </div>
-
+    <SignIn />
   );
 }
 
