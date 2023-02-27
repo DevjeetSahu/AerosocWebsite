122d1f22c67961c177ce5d871abee7d8db8cc1e2
Devjeet Sahu <111700112+DevjeetSahu@users.noreply.github.com>
2023-02-27 21:32:21 +0530

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,74 @@
+import "./footer.css"
+export default function footer() {
+  return (
+    <footer className="footer-distributed">
+
+      <div className="footer-left">
+        <h3>Useful links</h3>
+        <p className="footer-links">
+          <a href="http://www.iitkgp.ac.in/files/AcademicCalender2022_23ver2NEWFINAL.pdf" className="link-1">Academic Calendar</a>
+          <a href="#">Labs</a>
+          <a href="#">Previous Year Papers</a>
+          <a href="http://www.ae.iitkgp.ac.in/">Department Website</a>
+        </p>
+      </div>
+
+      <div className="footer-center">
+        <div>
+          <h3 className="contact">Contact</h3>
+          <i className="fa fa-map-marker"></i>
+          <p><span>Department of Aerospace Engineering, IIT Kharagpur</span>Kharagpur, India - 721302</p>
+        </div>
+        <div>
+          <i className="fa fa-phone"></i>
+          <p>Office : +91-3222-282241</p>
+        </div>
+      </div>
+
+      <div className="footer-right">
+        <div className="social">
+            <div className="icons-container">
+              
+                 <a href="#" target="_blank">
+                   <div className="icon github">
+                      <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 30 30">
+                        <path d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z"></path>
+                      </svg>
+                   </div>
+                 </a> 
+                 <a href="https://www.facebook.com/aeroiitkgp/" target="_blank">
+                   <div className="icon facebook">
+                   <svg xmlns="http://www.w3.org/2000/svg"x="0px" y="0px" width="50" height="50" viewBox="0 0 50 50">    
+                   <path d="M40,0H10C4.486,0,0,4.486,0,10v30c0,5.514,4.486,10,10,10h30c5.514,0,10-4.486,10-10V10C50,4.486,45.514,0,40,0z M39,17h-3 c-2.145,0-3,0.504-3,2v3h6l-1,6h-5v20h-7V28h-3v-6h3v-3c0-4.677,1.581-8,7-8c2.902,0,6,1,6,1V17z"></path>
+                   </svg>
+                   </div>
+                 </a> 
+
+                 <a href="#" target="_blank">
+                   <div className="icon instagram">
+                      <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="48" height="48" viewBox="0 0 24 24">
+                          <path d="M 8 3 C 5.243 3 3 5.243 3 8 L 3 16 C 3 18.757 5.243 21 8 21 L 16 21 C 18.757 21 21 18.757 21 16 L 21 8 C 21 5.243 18.757 3 16 3 L 8 3 z M 8 5 L 16 5 C 17.654 5 19 6.346 19 8 L 19 16 C 19 17.654 17.654 19 16 19 L 8 19 C 6.346 19 5 17.654 5 16 L 5 8 C 5 6.346 6.346 5 8 5 z M 17 6 A 1 1 0 0 0 16 7 A 1 1 0 0 0 17 8 A 1 1 0 0 0 18 7 A 1 1 0 0 0 17 6 z M 12 7 C 9.243 7 7 9.243 7 12 C 7 14.757 9.243 17 12 17 C 14.757 17 17 14.757 17 12 C 17 9.243 14.757 7 12 7 z M 12 9 C 13.654 9 15 10.346 15 12 C 15 13.654 13.654 15 12 15 C 10.346 15 9 13.654 9 12 C 9 10.346 10.346 9 12 9 z"></path>
+                      </svg>
+                   </div>
+                 </a> 
+
+                 <a href="#" target="_blank">
+                   <div className="icon twitter">
+                      <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 30 30">
+                        <path d="M28,6.937c-0.957,0.425-1.985,0.711-3.064,0.84c1.102-0.66,1.947-1.705,2.345-2.951c-1.03,0.611-2.172,1.055-3.388,1.295 c-0.973-1.037-2.359-1.685-3.893-1.685c-2.946,0-5.334,2.389-5.334,5.334c0,0.418,0.048,0.826,0.138,1.215 c-4.433-0.222-8.363-2.346-10.995-5.574C3.351,6.199,3.088,7.115,3.088,8.094c0,1.85,0.941,3.483,2.372,4.439 c-0.874-0.028-1.697-0.268-2.416-0.667c0,0.023,0,0.044,0,0.067c0,2.585,1.838,4.741,4.279,5.23 c-0.447,0.122-0.919,0.187-1.406,0.187c-0.343,0-0.678-0.034-1.003-0.095c0.679,2.119,2.649,3.662,4.983,3.705 c-1.825,1.431-4.125,2.284-6.625,2.284c-0.43,0-0.855-0.025-1.273-0.075c2.361,1.513,5.164,2.396,8.177,2.396 c9.812,0,15.176-8.128,15.176-15.177c0-0.231-0.005-0.461-0.015-0.69C26.38,8.945,27.285,8.006,28,6.937z"></path>
+                      </svg>
+                   </div>
+                 </a>
+                </div> 
+          </div>
+      </div>
+
+      <div className="copyright">
+        <p>Copyright &copy;
+        2023 All rights reserved | Aerospace Engineering Society, IIT Kharagpur</p>
+      </div>
+
+    </footer>
+  );
+
+}
